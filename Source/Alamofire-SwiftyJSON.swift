717af7e71c744286eb1353119ce9a7815ff8dc2f
tangplin <tangplin@gmail.com>
2014-09-26 13:47:38 +0800

    Adding documentation

--- Source/Alamofire-SwiftyJSON.swift
+++ Source/Alamofire-SwiftyJSON.swift
@@ -11,22 +11,31 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
-// MARK: - Request JSON
-
-//public func requestJSON(method: Alamofire.Method, URLString: Alamofire.URLStringConvertible, parameters: [String: AnyObject]? = nil) -> Alamofire.Request {
-//    return Alamofire.request(method, URLString , parameters: parameters, encoding: ParameterEncoding.JSON)
-//}
-
-// MARK: - Request
+// MARK: - Request for Swift JSON
 
 extension Alamofire.Request {
-
-    // MARK: - Swift JSON
     
+    /**
+    Adds a handler to be called once the request has finished.
+    
+    :param: completionHandler A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
+    
+    :returns: The request.
+    */
     public func responseSwiftyJSON(completionHandler: (NSURLRequest, NSHTTPURLResponse?, SwiftyJSON.JSON, NSError?) -> Void) -> Self {
         return responseSwiftyJSON(completionHandler: completionHandler)
     }
-
+    
+    /**
+    Adds a handler to be called once the request has finished.
+    
+    :param: priority The dispatch priority / quality of service used to process the response handler. `DISPATCH_QUEUE_PRIORITY_DEFAULT` by default.
+    :param: queue The queue on which the completion handler is dispatched.
+    :param: options The JSON serialization reading options. `.AllowFragments` by default.
+    :param: completionHandler A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
+    
+    :returns: The request.
+    */
     public func responseSwiftyJSON(priority: Int = DISPATCH_QUEUE_PRIORITY_DEFAULT, queue: dispatch_queue_t? = nil, options: NSJSONReadingOptions = .AllowFragments, completionHandler: (NSURLRequest, NSHTTPURLResponse?, SwiftyJSON.JSON, NSError?) -> Void) -> Self {
         
         return response(priority: priority, queue: queue, serializer: Alamofire.Request.JSONResponseSerializer(options: options), completionHandler: { (request, response, object, error) in
@@ -34,10 +43,14 @@ extension Alamofire.Request {
                 
                 var responseJSON: SwiftyJSON.JSON
                 
-                if (error != nil) {
+                if error != nil {
                     responseJSON = SwiftyJSON.JSON.Null(error)
                 } else {
-                    responseJSON = SwiftyJSON.JSON(object: object!)
+                    if object != nil {
+                        responseJSON = SwiftyJSON.JSON(object: object)
+                    } else {
+                        responseJSON = SwiftyJSON.JSON.Null(nil)
+                    }
                 }
                 
                 dispatch_async(queue ?? dispatch_get_main_queue(), {
