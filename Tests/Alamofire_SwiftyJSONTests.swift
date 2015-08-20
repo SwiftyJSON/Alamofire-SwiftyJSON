07060e155ef7686dee07ce351faec47e197b96ba
Julien Regnauld <j.regnauld@gmail.com>
2015-08-19 18:04:48 -0600

    Alamofire-SwiftyJSON for swift 2.0
    Switching  master branch of Alamofire and SwiftyJSON to swift 2.0 branch
    Update Alamofire-SwiftyJSON code in swift 2.0

--- Tests/Alamofire_SwiftyJSONTests.swift
+++ Tests/Alamofire_SwiftyJSONTests.swift
@@ -17,16 +17,14 @@ class Alamofire_SwiftyJSONTests: XCTestCase {
     func testJSONResponse() {
         let URL = "http://httpbin.org/get"
         let expectation = expectationWithDescription("\(URL)")
-        
-        Alamofire.request(.GET, URL, parameters: ["foo": "bar"])
-            .responseSwiftyJSON { (request, response, responseJSON, error) in
-                expectation.fulfill()
-                XCTAssertNotNil(request, "request should not be nil")
-                XCTAssertNotNil(response, "response should not be nil")
-                XCTAssertNil(error, "error should be nil")
-                XCTAssertEqual(responseJSON["args"], SwiftyJSON.JSON(["foo": "bar"] as NSDictionary), "args should be equal")
-        }
-        
+
+        Alamofire.request(.GET, URL, parameters: ["foo": "bar"]).responseSwiftyJSON({(request, response, responseJSON, error) in
+            expectation.fulfill()
+            XCTAssertNotNil(request, "request should not be nil")
+            XCTAssertNotNil(response, "response should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            XCTAssertEqual(responseJSON["args"], SwiftyJSON.JSON(["foo": "bar"] as NSDictionary), "args should be equal")
+        })
         waitForExpectationsWithTimeout(10) { (error) in
             XCTAssertNil(error, "\(error)")
         }
