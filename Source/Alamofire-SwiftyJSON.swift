8cb1e134b28efef6d29a8f1682acbb850b7f8630
Julien Regnauld <j.regnauld@gmail.com>
2015-09-27 11:20:49 +0200

    Fixes #35, Update response for Alamofire 3. completionHandler returns a Response struct instead of  request, response, result.

--- Source/Alamofire-SwiftyJSON.swift
+++ Source/Alamofire-SwiftyJSON.swift
@@ -37,20 +37,23 @@ extension Request {
     */
     public func responseSwiftyJSON(queue: dispatch_queue_t? = nil, options: NSJSONReadingOptions = .AllowFragments, completionHandler: (NSURLRequest, NSHTTPURLResponse?, JSON, ErrorType?) -> Void) -> Self {
         
-        return response(queue: queue, responseSerializer: Request.JSONResponseSerializer(options: options), completionHandler: { (request, response, result) -> Void in
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-                var responseJSON: JSON
-                if result.isFailure
-                {
-                    responseJSON = JSON.null
-                } else {
-                    responseJSON = SwiftyJSON.JSON(result.value!)
-                }
-                dispatch_async(queue ?? dispatch_get_main_queue(), {
-                    completionHandler(self.request!, self.response, responseJSON, result.error)
+        // With Alamofire 3, completionHandler returns a Response struct instead of request, response, result.
+        //For more information about Response struct see: https://github.com/Alamofire/Alamofire/blob/master/Documentation/Alamofire%203.0%20Migration%20Guide.md
+        return response(queue: queue, responseSerializer: Request.JSONResponseSerializer(options: options), completionHandler: { (response) -> Void in
+                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+                     var responseJSON: JSON
+                    if response.result.isFailure
+                    {
+                        responseJSON = JSON.null
+                    } else {
+                        responseJSON = SwiftyJSON.JSON(response.result.value!)
+                    }
+                    dispatch_async(queue ?? dispatch_get_main_queue(), {
+                        completionHandler(response.request!, response.response, responseJSON, response.result.error)
+                    })
                 })
             })
-        })
+        
     }
 }
 
